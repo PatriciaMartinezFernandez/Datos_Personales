0f40dfea15f679254a9d1fc352be63298d6f242c
JaimeMartinez <martinez.fernandez.patricia@iescamas.es>
2024-10-08 18:00:31 +0200

    Added method cambiarFoco()

--- app/src/main/java/com/jaimito/datospersonales/MainActivity.java
+++ app/src/main/java/com/jaimito/datospersonales/MainActivity.java
@@ -1,24 +1,52 @@
 package com.jaimito.datospersonales;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.appcompat.widget.SwitchCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText[] editTexts = new EditText[4];
+    ImageButton ivUpArrow, ivDownArrow;
+    SwitchCompat switchInfoLog;
+    int currentFoco;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        editTexts[0] = findViewById(R.id.etNombre);
+        editTexts[1] = findViewById(R.id.etApellidos);
+        editTexts[2] = findViewById(R.id.etEmail);
+        editTexts[3] = findViewById(R.id.etTelefono);
+        ivUpArrow = findViewById(R.id.ivUpArrow);
+        ivDownArrow = findViewById(R.id.ivDownArrow);
+        switchInfoLog = findViewById(R.id.switchInfoLog);
+
+        editTexts[0].requestFocus();
+        currentFoco = 0;
+
+        ivUpArrow.setOnClickListener(v -> { cambiarFoco(-1); });
+        ivDownArrow.setOnClickListener(v -> { cambiarFoco(1); });
     }
+
+    private void cambiarFoco(int direccion) {
+        editTexts[currentFoco].clearFocus();
+        currentFoco = (currentFoco + direccion + editTexts.length) % editTexts.length;
+        editTexts[currentFoco].requestFocus();
+
+        if (switchInfoLog.isChecked()) {
+            Log.i("currentFoco", "El foco es: " + currentFoco + ": " + editTexts[currentFoco].getText().toString());
+        }
+
+    }
+
 }
